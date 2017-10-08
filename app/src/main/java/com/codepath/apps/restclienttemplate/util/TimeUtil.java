e781925a0b118db7dd07fb2ac4ad3f5b492b0430
Valli Vidhya Venkatesan <vallividhyavenkatesan@gmail.com>
2017-10-08 00:12:08 -0700

    Shorten date format

--- app/src/main/java/com/codepath/apps/restclienttemplate/util/TimeUtil.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/util/TimeUtil.java
@@ -22,8 +22,18 @@ public class TimeUtil {
         String relativeDate = "";
         try {
             long dateMillis = sf.parse(rawJsonDate).getTime();
-            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
-                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE).toString();
+            long elapsed = System.currentTimeMillis() - dateMillis;
+            if (elapsed < 60000) {
+                return (elapsed / 1000) + "s" ;
+            } else if (elapsed < 3600000) {
+                return (elapsed / (1000 * 60)) + "m";
+            } else if (elapsed < 86400000) {
+                return (elapsed / (1000 * 60 * 60)) + "h";
+            } else if (elapsed < 604800000) {
+                return (elapsed / (1000 * 60 * 60 * 24)) + "d";
+            } else if (elapsed < 2419200000L) {
+                return (elapsed / (1000 * 60 * 60 * 24 * 7)) + "w";
+            }
         } catch (ParseException e) {
             e.printStackTrace();
         }
